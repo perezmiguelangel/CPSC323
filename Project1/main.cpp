083f498a5f813e9fe1bd863885f89ec2554bfbba
Miguel Angel Perez <PerezMiguelAngel@proton.me>
2024-10-08 13:36:11 -0700

    Added more to parser to cleanup code, still working out bugs

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 //To read from file
 #include <fstream>
+#include <vector>
+
+#define MAX_LINES 30
 
 using namespace std;
 
@@ -18,32 +21,43 @@ int main()
   ifstream myFile ("code.txt");
   string buffer;
   vector<string> code;
+  code.resize(MAX_LINES);
+  string buf;
+  
   if(myFile.is_open())
     {
+      int j = 0;
+      bool backspace = false;
       while(getline(myFile, buffer))
 	{
+	  backspace = false;
 	  //Step 2: Remove excess space and comments
 	  for(int i = 0; i < buffer.size(); i++)
 	    {
 	      //If char == /, is comment, skip
 	      if(buffer[i] == '/')
 		{
-		  //Do nothing (Will not add to final code)
-		  
+		  //Breaking early from for loop since whole
+		  //line is comment
+		  break;
 		}
 	      //If char is whitespace
 	      else if(isspace(buffer[i]))
 		{
-		  
+		  //ignore?
 		}
 	      //Char is normal char, write to code
 	      else
 		{
-
+		  code[j].push_back(buffer[i]);
 		}
 	    }
-	  cout << buffer << endl;
-	}
+	  
+	  code[j].push_back('\n');
+	  ++j;
+	   
+	  
+        }
       myFile.close();
     }
   else
@@ -52,6 +66,19 @@ int main()
     }
 
 
+  //Vector of strings, code, has cleaned up code
+  for(int i = 0; i < code.size(); i++)
+    {
+      if(code[i].compare("\n") == 0)
+	{
+	  code.erase(code.begin() + i);
+	}
+    }
+  for(string s : code)
+    {
+      cout << s;
+    }
+  
 
   return 0;
 }
